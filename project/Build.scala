a97b5968b689761534da85311dd28736c16f0fe0
Andrei Pozolotin <andrei.pozolotin@gmail.com>
2018-01-29 08:03:22 -0600

    use 2.12

--- project/Build.scala
+++ project/Build.scala
@@ -119,7 +119,7 @@ object Build {
   }
 
   val commonSettings = Seq(
-      scalaVersion := "2.11.12",
+      scalaVersion := "2.12.4",
       organization := "org.scala-js",
       version := scalaJSVersion,
 
